0e61ce53cdd8c7e258eff25286be2d095ad12a6a
Abdurahmon007 <elbekmovlonov@gmail.com>
2024-08-29 18:44:13 +0500

    added proper icon, navbar changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./navbar.css";
+import Notification from "../../img/notification.svg";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <span className="logo">Lama App</span>
+      <div className="icons">
+        <div className="icon">
+          <img src={Notification} alt="" className="iconImg" />
+          <div>2</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
